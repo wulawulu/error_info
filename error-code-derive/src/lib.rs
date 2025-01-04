4e14561cabe735e2e2c0c39da08d636cf88671f5
wulawulu <w2473698581@163.com>
2025-01-04 18:14:38 +0800

    feature: support ToErrorInfo macro

--- error-code-derive/src/lib.rs
+++ error-code-derive/src/lib.rs
@@ -1,1 +1,10 @@
-// empty file
+mod error_info;
+
+use crate::error_info::process_error_info;
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(ToErrorInfo, attributes(error_info))]
+pub fn derive_auto_debug(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    process_error_info(input).into()
+}
--- error-code/src/lib.rs
+++ error-code/src/lib.rs
@@ -1,1 +1,36 @@
-// empty file
+use std::fmt;
+use std::str::FromStr;
+
+pub use error_code_derive::ToErrorInfo;
+
+#[derive(Debug)]
+pub struct ErrorInfo<T> {
+    pub app_code: T,
+    pub code: &'static str,
+    pub client_msg: &'static str,
+    pub server_msg: String,
+}
+
+pub trait ToErrorInfo {
+    type T: FromStr;
+    fn to_error_info(&self) -> Result<ErrorInfo<Self::T>, <Self::T as FromStr>::Err>;
+}
+
+impl<T> ErrorInfo<T>
+where
+    T: FromStr,
+{
+    pub fn try_new(
+        app_code: &str,
+        code: &'static str,
+        client_msg: &'static str,
+        server_msg: impl fmt::Display,
+    ) -> Result<Self, T::Err> {
+        Ok(Self {
+            app_code: T::from_str(app_code)?,
+            code,
+            client_msg,
+            server_msg: server_msg.to_string(),
+        })
+    }
+}
